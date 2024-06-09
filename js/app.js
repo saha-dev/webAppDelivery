8bee4783d56fe2265548c876854c56c83b4342d6
saha <acidoralkali@gmail.com>
2024-06-10 00:30:25 +0300

    btn amaunt

--- js/app.js
+++ js/app.js
@@ -4,3 +4,8 @@ tg.expand();
 
 tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
+
+const btns = document.querySelectorAll('.btn-buy');
+btns.forEach((btn) => {
+    btn.addEventListener('click', () => {});
+});
